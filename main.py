93b82e31fbea83e2abc2a969e086e75b96ddbac0
diego <11331227@gafe.cksh.tp.edu.tw>
2025-08-05 12:41:43 +0800

    add layer

--- main.py
+++ main.py
@@ -1,5 +1,49 @@
 import gymnasium as gym
 import matplotlib.pyplot as plt
-import seaborn
+import seaborn as sns
 import numpy as np
 import torch 
+from torch import nn
+from torch import optim
+from torch.distributions.categorical import Categorical
+
+sns.set()
+
+DEVICE= 'gpu'
+
+# Policy and value model
+class ActorCriticNetwork(nn.Module):
+  def __init__(self, obs_space_size, action_space_size):
+    super().__init__()
+
+    self.shared_layers = nn.Sequential(
+        nn.Linear(obs_space_size, 64),
+        nn.ReLU(),
+        nn.Linear(64, 64),
+        nn.ReLU())
+    
+    self.policy_layers = nn.Sequential(
+        nn.Linear(64, 64),
+        nn.ReLU(),
+        nn.Linear(64, action_space_size))
+    
+    self.value_layers = nn.Sequential(
+        nn.Linear(64, 64),
+        nn.ReLU(),
+        nn.Linear(64, 1))
+    
+  def value(self, obs):
+    z = self.shared_layers(obs)
+    value = self.value_layers(z)
+    return value
+        
+  def policy(self, obs):
+    z = self.shared_layers(obs)
+    policy_logits = self.policy_layers(z)
+    return policy_logits
+
+  def forward(self, obs):
+    z = self.shared_layers(obs)
+    policy_logits = self.policy_layers(z)
+    value = self.value_layers(z)
+    return policy_logits, value
